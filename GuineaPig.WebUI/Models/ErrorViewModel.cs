6517d3a51cebba1e2e3fafad6f62cb7d121c1f18
Eugenio Miró <eugenio.miro@neoris.com>
2019-12-14 18:39:48 -0300

    clean up files

--- GuineaPig.WebUI/Models/ErrorViewModel.cs
+++ GuineaPig.WebUI/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace GuineaPig.WebUI.Models
 {
     public class ErrorViewModel
--- GuineaPig.WebUI/Startup.cs
+++ GuineaPig.WebUI/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GuineaPig.DataAccess.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using GuineaPig.DataAccess.Data;
 
 namespace GuineaPig.WebUI
 {

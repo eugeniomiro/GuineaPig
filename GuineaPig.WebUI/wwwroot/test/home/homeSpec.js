9a379e3df4f9eba8da98846ac31068864384eb4f
Eugenio Miró <eugenio.miro@neoris.com>
2019-12-16 00:15:47 -0300

    update testing infrastructure

--- GuineaPig.WebUI/wwwroot/test/home/homeSpec.js
+++ GuineaPig.WebUI/wwwroot/test/home/homeSpec.js
@@ -1,4 +1,10 @@
 ﻿describe("the home page", function () {
+
+  beforeAll(function () { });
+  beforeEach(function () { });
+  afterEach(function () { });
+  afterAll(function () { });
+
   it('should work', function () {
     expect(true).toBeTruthy();
 

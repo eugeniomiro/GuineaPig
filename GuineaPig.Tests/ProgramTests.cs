887a2654228c808f0afb3a4302de9003407f4853
Eugenio Miró <eugenio.miro@neoris.com>
2020-10-12 12:52:21 -0300

    Add Program tests

---
+++ GuineaPig.Tests/ProgramTests.cs
@@ -0,0 +1,61 @@
+﻿using GuineaPig.DataAccess.Data;
+using GuineaPig.Tests.Specs;
+using GuineaPig.WebUI.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuineaPig.WebUI.Tests
+{
+    [TestClass]
+    public class ProgramTests : AAA
+    {
+        [TestClass]
+        public class Calling_CreatHostBuilder_With_No_Args : ProgramTests
+        {
+            protected IHostBuilder sut;
+
+            protected override void Act()
+            {
+                sut = Program.CreateHostBuilder(new string[0]);
+            }
+
+            [TestMethod()]
+            public void Configures_Startup()
+            {
+                // assert
+                Assert.AreEqual(typeof(Startup), sut.Properties["UseStartup.StartupType"]);
+            }
+        }
+
+        [TestClass]
+        public class Building_Host_With_No_Args_On_Create : ProgramTests
+        {
+            private IHost host;
+
+            protected override void Act()
+            {
+                host = Program.CreateHostBuilder(new string[0]).Build();
+            }
+
+            [TestMethod]
+            public void Returns_Not_Null_Host()
+            {
+                Assert.IsNotNull(host);
+            }
+
+            [TestMethod]
+            public void Returns_Configured_DataContext()
+            {
+                Assert.IsNotNull(host.Services.GetService<ApplicationDbContext>());
+            }
+
+            [TestMethod]
+            public void Returns_Configured_ILogger_For_Controllers()
+            {
+                Assert.IsNotNull(host.Services.GetService<ILogger<HomeController>>());
+            }
+        }
+    }
+}
---
+++ GuineaPig.Tests/Specs/AAA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuineaPig.Tests.Specs
+{
+    [TestClass]
+    public class AAA
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Arrange();
+            Act();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            CleanUp();
+        }
+
+        protected virtual void Act() { }
+        protected virtual void Arrange() { }
+        protected virtual void CleanUp() { }
+
+    }
+}
